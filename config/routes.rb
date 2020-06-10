087098961555605649bd7c40428f477367c06274
加藤明 <akira_pc@katouakiranoMacBook-Pro.local>
2020-06-10 23:07:34 +0900

    update heroku

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
 #  get "home/:id/:upper/:lower" => "home#show"
 
-  get "post/:id" => "home#show"
+#  get "post/:id" => "home#show"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
